ce310036adf833b32182bef7922181177ebc4e4d
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2019-11-28 11:29:24 +0100

    Improve minipot-init with configuration file manipulation to set correct IP addresses

--- bin/minipot-init.sh
+++ bin/minipot-init.sh
@@ -1,5 +1,52 @@
 #!/bin/sh
 
+print_syntax ()
+{
+	echo "Syntax: $0 -i IPADDRESS"
+	echo "Please use the IP address of the machine where minipot is supposed to run"
+}
+
+# $1 the IP address
+_inject_ip()
+{
+	local ip="$1"
+	# configure consul
+	sysrc consul_enable="-client $ip -advertise $ip"
+	# configure nomad
+	sed -i '' -e "s/[:blank:]*http = \".*:4646\"$/  http = \"$ip:4646\"/" /usr/local/etc/nomad/server.hcl
+	sed -i '' -e "s/[:blank:]*address = \".*:8500\"$/  http = \"$ip:8500\"/" /usr/local/etc/nomad/server.hcl
+	# configure traefik
+	sed -i '' -e "s/[:blank:]*address = \".*:8080\"$/    address = \"$ip:8080\"/" /usr/local/etc/traefik.toml
+	sed -i '' -e "s/[:blank:]*address = \".*:9002\"$/    address = \"$ip:9002\"/" /usr/local/etc/traefik.toml
+	sed -i '' -e "s/[:blank:]*endpoint = \".*:8500\"$/  endpoint = \"$ip:8500\"/" /usr/local/etc/traefik.toml
+}
+
+if [ "$#" -lt 2 ]; then
+	print_syntax
+	exit 1
+fi
+
+while getopts :i:h arg; do
+	case $arg in
+		h)
+			print_syntax
+			exit 0
+			;;
+		i)
+			ip="$OPTARG"
+			;;
+		?)
+			print_syntax
+			exit 1
+			;;
+	esac
+done
+
+if [ -z "$ip" ]; then
+	print_syntax
+	exit 1
+fi
+
 mkdir -p /usr/local/etc/consul.d
 sysrc nomad_user="root"
 sysrc nomad_env="PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/sbin:/bin"
